d02cefcd8ef54876a75b1af2c9fc7bd0b31d8941
Zoltanbi <zoltanbiro001@gmail.com>
2022-12-02 12:54:11 -0800

    setup client

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+function App() {
+  return (
+    <div>
+      Kanban app
+    </div>
+  );
+}
+
+export default App;
