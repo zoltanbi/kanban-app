db1717f84456c98855d4942065f1ea48b5680a33
Zoltanbi <zoltanbiro001@gmail.com>
2022-12-03 10:08:09 -0800

    setup user API

---
+++ server/src/v1/controllers/user.js
@@ -0,0 +1,71 @@
+const User = require('../models/user');
+const CryptoJS = require('crypto-js');
+const jsonwebtoken = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    const { password } = req.body;
+    try {
+        req.body.password = CryptoJS.AES.encrypto(
+            password,
+            process.env.PASSWORD_SECRET_KEY
+        );
+
+        const user = await User.create(req.body);
+        const token = jsonwebtoken.sign(
+            { id: user._id },
+            process.env.TOKEN_SECRET_KEY,
+            { expiresIn: '24h'}
+        );
+        res.status(201).json({ user, token });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username }).select('password username');
+        if (!user) {
+            return res.status(401).json({
+                errors: [
+                    {
+                        param: 'username',
+                        msg: 'Invalid username or password'
+                    }
+                ]
+            });
+        };
+
+        const decryptPass = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASSWORD_SECRET_KEY
+        ).toString(CryptoJS.enc.Utf8)
+
+        if (decryptPass !== password) {
+            return res.status(401).json({
+                errors: [
+                    {
+                        param: 'password',
+                        msg: 'Invalid username or password'
+                    }
+                ]
+            });
+        }
+
+        user.password = undefined;
+
+        const token = jsonwebtoken.sign(
+            { id: user._id },
+            process.env.TOKEN_SECRET_KEY,
+            { expiresIn: '24h'}
+        );
+
+        res.status(200).json({ user, token })
+
+    } catch(err) {
+        res.status(500).json(err);
+    }
+}
